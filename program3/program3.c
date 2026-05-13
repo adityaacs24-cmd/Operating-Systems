baa55debeae81e92c0723ec6197c2f6c8407e9d9
Aditya A <adityaanil08@gmail.com>
2026-05-13 17:44:28 +0530

    commit 1

--- program3/program3.c
+++ program3/program3.c
@@ -1,43 +1,36 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-struct Process
-{
+struct Process {
     int pid;
     int burst;
     int period;
-    int deadline; 
+    int deadline;
     int remaining;
     int current_deadline;
-    int tickets; 
+    int tickets;
 };
 
-int gcd(int a, int b)
-{
+int gcd(int a, int b) {
     if (b == 0)
         return a;
     return gcd(b, a % b);
 }
 
-int lcm(int a, int b)
-{
+int lcm(int a, int b) {
     if (a == 0 || b == 0)
         return 0;
     return (a * b) / gcd(a, b);
 }
 
 // 1. Rate Monotonic Scheduling (RMS)
-void runRMS(struct Process p[], int n, int hyperPeriod)
-{
+void runRMS(struct Process p[], int n, int hyperPeriod) {
     printf("\n--- Running Rate Monotonic Scheduling ---\n");
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (p[j].period > p[j + 1].period)
-            {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].period > p[j + 1].period) {
                 struct Process temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
@@ -45,28 +38,25 @@ void runRMS(struct Process p[], int n, int hyperPeriod)
         }
     }
 
-    for (int t = 0; t < hyperPeriod; t++)
-    {
+    for (int t = 0; t < hyperPeriod; t++) {
         int chosen = -1;
-        for (int i = 0; i < n; i++)
-        {
+
+        for (int i = 0; i < n; i++) {
             if (t % p[i].period == 0)
                 p[i].remaining = p[i].burst;
         }
-        for (int i = 0; i < n; i++)
-        {
-            if (p[i].remaining > 0)
-            {
+
+        for (int i = 0; i < n; i++) {
+            if (p[i].remaining > 0) {
                 chosen = i;
                 break;
             }
         }
-        if (chosen != -1)
-        {
+        
+        if (chosen != -1) {
             printf("Time %d: P%d\n", t, p[chosen].pid);
             p[chosen].remaining--;
-        }
-        else
+        } else
             printf("Time %d: Idle\n", t);
     }
 }
@@ -74,11 +64,11 @@ void runRMS(struct Process p[], int n, int hyperPeriod)
 // 2. Earliest Deadline First (EDF)
 void runEDF(struct Process p[], int n, int hyperPeriod) {
     printf("\n--- Running Earliest Deadline First (Custom Deadlines) ---\n");
-    
+
     // Initialize first instances
     for (int i = 0; i < n; i++) {
         p[i].remaining = p[i].burst;
-        p[i].current_deadline = p[i].deadline; 
+        p[i].current_deadline = p[i].deadline;
     }
 
     for (int t = 0; t < hyperPeriod; t++) {
@@ -107,7 +97,8 @@ void runEDF(struct Process p[], int n, int hyperPeriod) {
         }
 
         if (chosen != -1) {
-            printf("Time %dms: P%d (Abs Deadline: %d)\n", t, p[chosen].pid, p[chosen].current_deadline);
+            printf("Time %dms: P%d (Abs Deadline: %d)\n", t, p[chosen].pid,
+                   p[chosen].current_deadline);
             p[chosen].remaining--;
         } else {
             printf("Time %dms: Idle\n", t);
@@ -115,27 +106,22 @@ void runEDF(struct Process p[], int n, int hyperPeriod) {
     }
 }
 
-void runProportionalShare(struct Process p[], int n, int totalTime)
-{
+void runProportionalShare(struct Process p[], int n, int totalTime) {
     printf("\n--- Running Proportional Share (Lottery) ---\n");
     int totalTickets = 0;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         printf("Enter tickets for P%d: ", p[i].pid);
         scanf("%d", &p[i].tickets);
         totalTickets += p[i].tickets;
     }
 
-    for (int t = 0; t < totalTime; t++)
-    {
+    for (int t = 0; t < totalTime; t++) {
         int lottery = rand() % totalTickets;
         int sum = 0, chosen = -1;
 
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             sum += p[i].tickets;
-            if (lottery < sum)
-            {
+            if (lottery < sum) {
                 chosen = i;
                 break;
             }
@@ -144,8 +130,7 @@ void runProportionalShare(struct Process p[], int n, int totalTime)
     }
 }
 
-int main()
-{
+int main() {
     int n, choice;
     printf("Enter the number of processes: ");
     if (scanf("%d", &n) != 1)
@@ -163,12 +148,11 @@ int main()
         scanf("%d", &p[i].period);
         printf("  Relative Deadline: ");
         scanf("%d", &p[i].deadline);
-        
+
         hyperPeriod = lcm(hyperPeriod, p[i].period);
     }
 
-    while (1)
-    {
+    while (1) {
         printf("\n--- Real-Time Scheduling Simulator ---\n");
         printf("1. Rate Monotonic (RMS)\n");
         printf("2. Earliest Deadline First (EDF)\n");
@@ -183,8 +167,7 @@ int main()
         for (int i = 0; i < n; i++)
             temp_p[i] = p[i];
 
-        switch (choice)
-        {
+        switch (choice) {
         case 1:
             runRMS(temp_p, n, hyperPeriod);
             break;
