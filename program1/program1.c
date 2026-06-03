6061c29ebb7b845dd0600423c3694251cdeef5fc
Aditya A <adityaanil08@gmail.com>
2026-06-03 18:40:24 +0530

    fix round robin scheduling logic

--- program1/program1.c
+++ program1/program1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 typedef struct {
-    int at, bt, ct, tat, wt;
+    int at, bt, ct, tat, wt, in_queue;
     int done, remaining_bt, priority;
 } Process;
 
@@ -174,30 +174,61 @@ void priority_p(Process p[], int n) {
 void round_robin(Process p[], int n, int tq) {
     int cp = 0, curr_time = 0;
 
+    // An array to serve as our FIFO queue storing process indices
+    // MAX_Q needs to be large enough to handle processes re-entering multiple times
+    int queue[100];
+    int front = 0, rear = 0;
+
+    // Initialize remaining burst time and queue tracking flags
     for (int i = 0; i < n; i++) {
         p[i].remaining_bt = p[i].bt;
+        p[i].in_queue = 0;
     }
 
     while (cp != n) {
-        int found = 0;
-
         for (int i = 0; i < n; i++) {
-            if (p[i].at <= curr_time && p[i].remaining_bt > 0) {
-                found = 1;
-
-                if (p[i].remaining_bt >= tq) {
-                    curr_time += tq;
-                    p[i].remaining_bt -= tq;
-                } else {
-                    curr_time += p[i].remaining_bt;
-                    p[i].remaining_bt = 0;
-                    p[i].ct = curr_time;
-                    cp++;
-                }
+            if (p[i].at <= curr_time && !p[i].in_queue && p[i].remaining_bt > 0) {
+                queue[rear++] = i;
+                p[i].in_queue = 1;
             }
         }
 
-        if (!found) {
+        // If the queue is not empty, process the next item
+        if (front < rear) {
+            int idx = queue[front++];  // Dequeue
+
+            printf("P%d ", idx + 1);
+
+            // Determine how long to run (either the full quantum or whatever is left)
+            int run_time = (p[idx].remaining_bt > tq) ? tq : p[idx].remaining_bt;
+
+            // Execute step-by-step (1 unit at a time) to catch mid-quantum arrivals
+            for (int t = 0; t < run_time; t++) {
+                curr_time++;
+                p[idx].remaining_bt--;
+
+                // Check for newly arrived processes during this time tick
+                for (int i = 0; i < n; i++) {
+                    if (p[i].at <= curr_time && !p[i].in_queue && p[i].remaining_bt > 0) {
+                        queue[rear++] = i;  // Enqueue new arrival
+                        p[i].in_queue = 1;
+                    }
+                }
+            }
+
+            p[idx].in_queue = 0;
+
+            // Check if the current process finished or needs to go to the back of the queue
+            if (p[idx].remaining_bt == 0) {
+                p[idx].ct = curr_time;
+                cp++;
+            } else {
+                queue[rear++] = idx;  // Enqueue back to the rear
+                p[idx].in_queue = 1;
+            }
+        }
+        // If the queue is empty, the CPU is idling
+        else {
             curr_time++;
         }
     }
@@ -224,29 +255,33 @@ void calc_avg(Process p[], int n, float* avg_tat, float* avg_wt) {
 int main() {
     float avg_tat, avg_wt;
 
-    Process p[5];
+    Process p[6];
     p[0].at = 0;
-    p[0].bt = 3;
+    p[0].bt = 4;
     p[0].priority = 5;
-    p[1].at = 2;
-    p[1].bt = 2;
+    p[1].at = 1;
+    p[1].bt = 5;
     p[1].priority = 3;
-    p[2].at = 3;
-    p[2].bt = 5;
+    p[2].at = 2;
+    p[2].bt = 2;
     p[2].priority = 2;
-    p[3].at = 4;
-    p[3].bt = 4;
+    p[3].at = 3;
+    p[3].bt = 1;
     p[3].priority = 4;
-    p[4].at = 6;
-    p[4].bt = 1;
+    p[4].at = 4;
+    p[4].bt = 6;
     p[4].priority = 1;
+    p[5].at = 6;
+    p[5].bt = 3;
+    p[5].priority = 1;
 
     // fcfs(p, 5);
     // sjf(p, 5);
     // srtf(p, 5);
-
-    priority_np(p, 5);
-    calc_avg(p, 5, &avg_tat, &avg_wt);
+    // priority_np(p, 5);
+    round_robin(p, 6, 2);
+    printf("\n\n");
+    calc_avg(p, 6, &avg_tat, &avg_wt);
 
     printf("Avg TAT: %.2f\n", avg_tat);
     printf("Avg WT: %.2f\n", avg_wt);
